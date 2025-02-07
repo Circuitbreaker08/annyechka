52602272e5fa58869ae51212cf548ec9f6a3f4da
Circuitbreaker08 <circuitbreaker08@gmail.com>
2025-02-07 16:48:19 -0500

    Added message

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var note
 
+var message =
+`Hello Anna
+As you can see,
+we have your Squid.
+We have simple demands
+that must be met.
+First, you will pay us in the amount of
+14€ and 57₽.
+Then you will give us the locker dino.
+Meet our demands within
+19.000001 years
+
+Byeeeeeeeeeeeeeee`
+
 window.onload = () => {
     note = document.getElementById("note")
-    note.innerHTML = "Hi <strong>:3</strong>"
+    console.log(note.innerHTML)
 }
