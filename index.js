bffd004af6b062b92d426575c4601e6a98c07579
Circuitbreaker08 <circuitbreaker08@gmail.com>
2025-02-07 17:11:50 -0500

    Implemented the text randomizer

--- index.js
+++ index.js
@@ -10,14 +10,37 @@ First, you will pay us in the amount of
 14€ and 57₽.
 Then you will give us the locker dino.
 Meet our demands within
-19.000001 years
+3.37 days
 
-Byeeeeeeeeeeeeeee`
+Byeeeeeeeeeeeeeee
+
+p.s. totally not anyone you know hehe`
+
+function randomFont() {
+    return [
+        "Arial",
+        "Verdana",
+        "Tahoma",
+        "Trebuchet MS",
+        "Times New Roman",
+        "Georgia",
+        "Garamond",
+        "Courier New",
+        "Brush Script MT"
+
+    ][Math.random() % 9]
+}
+
+function randomSize() {
+    return Math.random() % 20 + 9
+}
 
 window.onload = () => {
+    let out = ""
     for (i = 0; i < message.length; i++) {
-        console.log(message[i])
+        let char = ""
+        char += "<p style=\"font-family: " + randomFont() + "; font-size: " + randomSize() + "px\n></p>"
+        out += char
     }
-    //note = document.getElementById("note")
-    //console.log(note.innerHTML)
+    document.getElementById("note").innerHTML = out
 }
